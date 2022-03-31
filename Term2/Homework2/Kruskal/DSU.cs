569c426dd945c1e5f0c62cf7b846b77f887f3792
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-03-31 16:52:52 +0300

    Impemented DSU

---
+++ Term2/Homework2/Kruskal/DSU.cs
@@ -0,0 +1,53 @@
+namespace Kruskal;
+
+public class DSU
+{
+	private int[] parent;
+	private int[] rank;
+
+	public DSU(int n) 
+	{
+		parent = new int[n];
+		rank = new int[n];
+
+		for (int i = 0; i < n; i++) 
+		{
+			parent[i] = i;
+			rank[i] = 1;
+		}
+	}
+
+	public int GetParent(int v) {
+		if (parent[v] == v) 
+			return v;
+		
+		return parent[v] = GetParent(v);
+	}
+
+	public bool JoinSets(int v, int u) 
+	{
+		u = GetParent(u);
+		v = GetParent(v);
+
+		if (u == v)
+		{
+			return true;
+		}
+
+		if (rank[u] < rank[v]) 
+		{
+			parent[u] = v;
+		}
+		else
+		{
+			parent[v] = u;
+		}
+
+		if (rank[u] == rank[v])
+		{
+			rank[v]++;
+		}
+		
+		return false;
+	}
+}
