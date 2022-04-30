8dd564ce019d4b60cc0a6fc99f28bc6cd3e4bba9
Ilya Dudnikov <dudnikoff98@gmail.com>
2022-04-30 18:08:01 +0300

    Add trivial implementation of Prim's algorithm

---
+++ Term2/Homework2/Prim/PrimTrivial/src/main/java/PrimSolver.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+
+public class PrimSolver {
+	private static final int INF = 1_000_000_000 + 23;
+	private static final int START_VERTICE = 0;
+
+	private int verticesCount;
+	protected int[][] graph;
+
+	public PrimSolver(int[][] graph) {
+		this.graph = graph;
+		this.verticesCount = graph.length;
+
+		for (var row : graph) {
+			if (row.length != verticesCount) {
+				throw new IllegalArgumentException("Graph should be given as an adjacency matrix");
+			}
+		}
+	}
+
+	private int[][] createEmptyGraph(int vertices) {
+		int[][] graph = new int[vertices][vertices];
+		for (int i = 0; i < vertices; i++) {
+			for (int j = 0; j < vertices; j++)
+				graph[i][j] = -1;
+		}
+
+		return graph;
+	}
+
+	public int[][] getMST() {
+		boolean used[] = new boolean[verticesCount];
+		int minDistance[] = new int[verticesCount];
+		int parent[] = new int[verticesCount];
+		Arrays.fill(minDistance, INF);
+		Arrays.fill(parent, -1);
+		minDistance[START_VERTICE] = 0;
+
+		int[][] mstGraph = createEmptyGraph(verticesCount);
+
+		for (int i = 0; i < verticesCount; i++) {
+			int currentVertice = -1;
+			for (int j = 0; j < verticesCount; j++) {
+				if (!used[j] && (currentVertice == -1 || minDistance[j] < minDistance[currentVertice])) {
+					currentVertice = j;
+				}
+			}
+
+			if (minDistance[currentVertice] == INF) {
+				throw new IllegalArgumentException("Given graph isn't connected");
+			}
+
+			used[currentVertice] = true;
+			if (i > 0) {
+				mstGraph[parent[currentVertice]][currentVertice] = graph[parent[currentVertice]][currentVertice];
+				mstGraph[currentVertice][parent[currentVertice]] = graph[currentVertice][parent[currentVertice]];
+			}
+			for (int j = 0; j < verticesCount; j++) {
+				if (graph[currentVertice][j] != -1 && graph[currentVertice][j] < minDistance[j]) {
+					minDistance[j] = graph[currentVertice][j];
+					parent[j] = currentVertice;
+				}
+			}
+		}
+
+		return mstGraph;
+	}
+
+	public int getMSTWeight() {
+		var mstGraph = getMST();
+		int mstWeight = 0;
+		for (int i = 0; i < verticesCount; i++) {
+			for (int j = 0; j < verticesCount; j++) {
+				if (mstGraph[i][j] != -1)
+					mstWeight += mstGraph[i][j];
+			}
+		}
+
+		return mstWeight / 2;
+	}
+}
---
+++ Term2/Homework2/Prim/PrimTrivial/src/test/java/PrimSolverTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimSolverTest {
+	@Test
+	void getMST() {
+		int[][] graph = new int[][]{
+				{-1, 1, -1, -1, 9, -1, -1},
+				{1, -1, 7, -1, -1, -1, 8},
+				{-1, 7, -1, 4, -1, -1, 10},
+				{-1, -1, 4, -1, 3, 5, -1},
+				{9, -1, -1, 3, -1, 2, -1},
+				{-1, -1, -1, 5, 2, -1, 6},
+				{-1, 8, 10, -1, -1, 6, -1}
+		};
+
+		PrimSolver primSolver = new PrimSolver(graph);
+		var mstGraph = primSolver.getMST();
+		int[][] expected = new int[][] {
+				{-1, 1, -1, -1, -1, -1, -1},
+				{1, -1, 7, -1, -1, -1, -1},
+				{-1, 7, -1, 4, -1, -1, -1},
+				{-1, -1, 4, -1, 3, -1, -1},
+				{-1, -1, -1, 3, -1, 2, -1},
+				{-1, -1, -1, -1, 2, -1, 6},
+				{-1, -1, -1, -1, -1, 6, -1}
+		};
+
+		for (int row = 0; row < graph.length; row++) {
+			for (int col = 0; col < graph.length; col++) {
+				assertEquals(expected[row][col], mstGraph[row][col]);
+			}
+		}
+	}
+
+	@Test
+	void getMSTWeight() {
+		int[][] graph = new int[][]{
+				{-1, 1, -1, -1, 9, -1, -1},
+				{1, -1, 7, -1, -1, -1, 8},
+				{-1, 7, -1, 4, -1, -1, 10},
+				{-1, -1, 4, -1, 3, 5, -1},
+				{9, -1, -1, 3, -1, 2, -1},
+				{-1, -1, -1, 5, 2, -1, 6},
+				{-1, 8, 10, -1, -1, 6, -1}
+		};
+
+		PrimSolver primSolver = new PrimSolver(graph);
+		assertEquals(23, primSolver.getMSTWeight());
+	}
+}
